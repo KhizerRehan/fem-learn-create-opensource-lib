cc5538419346a6748d78a175af6a976272fe1744
khizerrehan <khizer.rehan@aurorasolutions.io>
2019-12-22 22:57:36 +0500

    change const to var

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const starwarsNames = require("./starwars.json");
-const uniqueRandomArray = require("unique-random-array");
+"use strict";
+var starwarsNames = require("./starwars.json");
+var uniqueRandomArray = require("unique-random-array");
 
 module.exports = {
 	all: starwarsNames,
